a5bbd3e5041cb9dd274dc68e71dfe8a5701a24d6
KirAndStas <vesground@gmail.com>
2017-12-12 14:21:35 +0100

    finished with anchors. Done motLett part

--- client/components/sections/letter/index.jsx
+++ client/components/sections/letter/index.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes            from 'prop-types';
 
-import './Letter.less';
+import './index.less';
 
 export default class Letter extends Component {
     render() {
         return (
-            <div className='Letter'>
+            <div id='motLett' className='Letter'>
+            	<div className='title'>Mot lett</div>
+		            <p>What can do people with the same ideas/views together ? You know ? Just try imagine it ! </p>
+					<p>You with your friends. See it ? This feeling ? When we meet always do full of strange and undescriable things. Is it good or bad. Doesn`t matter. You enjoy it. I like it. I wait with impatience for it, counting days and hours. And when it end, I regret. Momment passed so fast. And we begin waiting and again counting till next one.</p>
+					<p>I think it is important to share, knowledge, minds or ideas. May be someone will argue it and you both come to something bigger than it was.</p>
+					<p>We find out people in interaction with each other. It is enough obvious. But less obvious is that we find in this way what to do, what else to try. May so we servive. At least I.</p>
+					<p>I wanna find what I will like to do, what fully swallow me up. And sure, I know a little bit about such thigns. I am crazy about design. When it is easy to understand how something work. It is kind of explanation. It is hard tool that helps in ours being. I like it, when it work. I wanna part of it. I am curious about how things work. Which affect on outward it has. Coding in some sense is a mirror. When you understand how each part run and what return. You begin to feel flow. And that you can control it. It is cool.</p>
+					<p>You can begin improve bugs in ours society. Sounds like rave. That is. Contact me, when you need is. Or just contact me.</p>
             </div>
         );
     }
