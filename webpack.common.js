1fdcbc3fff7fed4f1149a3a0f2fe46d677e15198
KirAndStas <vesground@gmail.com>
2017-12-29 18:49:42 +0100

    delete old

--- webpack.common.js
+++ webpack.common.js
@@ -18,14 +18,7 @@ module.exports = {
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
             { test: /\.less$/, loader: 'style-loader!css-loader!less-loader', exclude: /node_modules/ },
-            { test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/ },
-            // { test: /\.gif$/, loader: "url-loader?limit=10000&mimetype=image/gif" },
-            // { test: /\.jpg$/, loader: "url-loader?limit=10000&mimetype=image/jpg" },
-            // { test: /\.png$/, loader: 'file-loader', options: { name: '[name].[ext]' } },
-            // { test: /\.svg$/, loader: 'file-loader', options: { name: '[name].[ext]' } },
-            // { test: /\.(woff|woff2|ttf|eot)$/, loader: "url-loader?limit=1" }
-            // { test: /\.png$/, loader: "url-loader?limit=10000&mimetype=image/png" },
-            // { test: /\.svg$/, loader: "url-loader?limit=26000&mimetype=image/svg+xml" }
+            { test: /\.css$/, loader: 'style-loader!css-loader', exclude: /node_modules/ }
         ]
     },
     plugins : [ HtmlWebpackPluginConfig ]
